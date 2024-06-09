261bc002b1410db91ab8e95b2442af783bd8acb2
Darrian Lambert <Dtlambert1@buffs.wtamu.edu>
2024-06-09 10:57:19 -0500

    reset

--- Models/testContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace TestClassTest.Models {
-    public class TestDbContext : DbContext {
-        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options) {}
-        public DbSet<TestClass> TestClasses {get; set;} = default!;
-    }
-}
