62434c18f2e9462557ac3dabef28d7914878b0d5
Darrian Lambert <Dtlambert1@buffs.wtamu.edu>
2024-06-13 12:49:36 -0500

    Homepage structure setup. More will be added later on once the UI is set.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,3 +14,5 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - Updated README.md on coding procedures
+
+### On this Branch
--- obj/Debug/net8.0/CommunityLink.AssemblyInfo.cs
+++ obj/Debug/net8.0/CommunityLink.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommunityLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d31419b10b69f7cfda4c291de6bd941a350817e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486076aacef37be30f7f21c7aa856e93631f8da5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommunityLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommunityLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
