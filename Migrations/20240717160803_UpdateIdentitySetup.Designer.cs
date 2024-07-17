e4f50ce8519ec6525eace9b1af8ed180f64f30ca
Jzoll <hellojzoll@gmail.com>
2024-07-17 12:11:23 -0400

    workingIdentityScaffolding

--- Migrations/20240717160803_UpdateIdentitySetup.Designer.cs
+++ Migrations/20240717160803_UpdateIdentitySetup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CommunityLink.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CommunityLink.Migrations
 {
     [DbContext(typeof(CommunityLinkDbContext))]
-    partial class CommunityLinkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240717160803_UpdateIdentitySetup")]
+    partial class UpdateIdentitySetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
